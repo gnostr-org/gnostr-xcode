ebf7718680b1f9a603779002d46851444a074f1a
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-05-25 12:47:10 -0400

    AppWithTool/Script.sh: if [ ! -n DEBUG ]; ...

--- AppWithTool/Script.sh
+++ AppWithTool/Script.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-
 # Initialize global variables.
 {
   declare SCRIPT_INVOKED_NAME="${BASH_SOURCE[${#BASH_SOURCE[@]}-1]}"
@@ -14,60 +12,65 @@
   declare SCRIPT_RUN_DATE="$( date +%s )"
   ##export  SCRIPT_RUN_DATE
   
+if [ ! -n DEBUG ];then
   echo "\nSCRIPT_INVOKED_NAME="$SCRIPT_INVOKED_NAME
   echo "SCRIPT_NAME="$SCRIPT_NAME
   echo "SCRIPT_INVOKED_PATH="$SCRIPT_INVOKED_PATH
   echo "SCRIPT_PATH="$SCRIPT_PATH
   echo "SCRIPT_RUN_DATE="$SCRIPT_RUN_DATE
   echo "\n"
+fi
 
-ls $SCRIPT_INVOKED_PATH;echo "\n"
-open $SCRIPT_INVOKED_PATH/SanJuanGoddess.jpg
+if [ ! -n DEBUG ];then
+    ls $SCRIPT_INVOKED_PATH;
+    echo "\n"
+    open $SCRIPT_INVOKED_PATH/SanJuanGoddess.jpg
+fi
+
 $SCRIPT_INVOKED_PATH/./ToolX
 
 }
 
-## echo "Script.sh"
-## echo "\$(PWD)="$(PWD)
-## echo "/Users/"$(whoami)"/Library/Containers/dev.bitcoincore.AppWithTool/Data"
-
-## type -P bash
-## type -P ls && ls /bin && echo `pwd`
-
 addpath(){
+
+if [ ! -z DEBUG ];then
+    export PATH=$PATH:/Volumes/git/AppWithTool/Build/Products/Debug/
+else
+    echo
+fi
+
 export PATH=$PATH:$(PWD)
-export PATH=$PATH:/Volumes/git/AppWithTool/Build/Products/Debug/
 export PATH=$PATH:$SCRIPT_INVOKED_PATH
 echo $PATH
 }
 
-list(){
+list_user_home(){
 
-ls Desktop
-echo "\nDocuments:"
-ls Documents
-echo "\nDownloads:"
-ls Downloads
-open Downloads/SanJuanGoddess.jpg
-echo "\nLibrary:"
-ls Library
-echo "\nMovies:"
-ls Movies
-echo "\nMusic:"
-ls Music
-echo "\nPictures:"
-ls Pictures
-open Pictures/Photos\ Library.photoslibrary
-
+if [ ! -n DEBUG ];then
+    ## ls Desktop
+    echo "\nDocuments:"
+    ls Documents
+    echo "\nDownloads:"
+    ls Downloads
+    echo "\nLibrary:"
+    ls Library
+    echo "\nMovies:"
+    ls Movies
+    echo "\nMusic:"
+    ls Music
+    echo "\nPictures:"
+    ls Pictures
+    ## open Pictures/Photos\ Library.photoslibrary
+fi
 
 }
 
-ls $(PWD)
+## ls $(PWD)
 
 echo "DEBUG="$DEBUG
 
-if [ DEBUG ];then
-    addpath;
+if [ ! -n DEBUG ];then
+    list_user_home;
 else
     echo
 fi
