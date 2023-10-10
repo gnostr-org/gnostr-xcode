e03e4e9b00f3789b862d661bb29a980610543a4f
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-10-10 10:56:51 -0400

    ToolX/main.m

--- ToolX/main.m
+++ ToolX/main.m
@@ -15,18 +15,19 @@
 
 void report() {
 
-    printf("ToolX\n");
+    printf("ToolX:report()\n");
 
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char *argv[]) {
     @autoreleasepool {
-// report();
+    report();
 // int execv_bridge(const char *path, char *const argv[]);
     //execv_bridge(argv[0],(char *const *)argv);
-    execv_bridge(&argc,(char *const *)argv);
 
-// logargv(&argc, (char *const *)argv);
+    execv_bridge((const char *)argv, (char *const *)argv);
+
+        logargv(&argc, (char *const *)argv[0]);
 
     }
     return 0;
