bd2318710cc78caacb4e462fb9cfc4bd446420d8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-08 08:53:18 -0500

    xcode:initial commit

---
+++ gnostr/gnostrApp.swift
@@ -0,0 +1,17 @@
+//
+//  gnostrApp.swift
+//  gnostr
+//
+//  Created by Randy McMillan on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct gnostrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
